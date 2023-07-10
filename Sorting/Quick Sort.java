428c0bb2618a13b9235b10c72a3fb7bf0711f25a
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-07-10 13:41:07 +0530

    Create Quick Sort.java

---
+++ Sorting/Quick Sort.java
@@ -0,0 +1,35 @@
+//Time complexity
+//worst=O(n^2)
+//average=O(nlogn)
+//Worst case occurs when pivot is always 
+//the smallest or the largest element
+public static void quickSort(int arr[],int low,int high){
+  if(low<high){
+    int pi=partition(arr,low,high);
+
+  quickSort(arr,low,pi-);
+    quicksort(arr,pi+1,high);
+  }
+}
+
+public static int partition(int[] arr, int low,int high){
+  int pivot=arr[high];
+  int i=low-1;
+
+for(int j=low;j<high;j++){
+  if(arr[j]<pivot){
+    i++;
+    //swap
+  int temp=arr[i];
+    arr[i]=arr[j]
+      arr[j]=temp;
+  }
+}
+  //swap with pivot
+i++;
+  int temp=arr[i];
+  arr[i]=arr[high];
+  arr[high]=temp;
+  return i;
+}
+  
