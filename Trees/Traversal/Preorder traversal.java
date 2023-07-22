d14227b48a0a979a8aed0a8f584947b6452a109c
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-07-22 13:19:20 +0530

    Update Preorder traversal.java

--- Trees/Traversal/Preorder traversal.java
+++ Trees/Traversal/Preorder traversal.java
@@ -1,1 +1,11 @@
+public static void preorder(Node root) {
+       if(root == null) {
+           System.out.print(-1+" ");
+           return;
+       }
+       System.out.print(root.data+" ");
+       preorder(root.left);
+       preorder(root.right);
+   }
+
 
