f46064511d5c97cd982d7e93a1586bd3b9329ca4
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-07-23 08:03:00 +0530

    Update inorder traversal.java

--- Trees/Traversal/inorder traversal.java
+++ Trees/Traversal/inorder traversal.java
@@ -1,1 +1,10 @@
+public static void inorder(Node root) {
+       if(root == null) {
+           System.out.print(-1+" ");
+           return;
+       }
+       inorder(root.left);
+       System.out.print(root.data+" ");
+       inorder(root.right);
+   }
 
