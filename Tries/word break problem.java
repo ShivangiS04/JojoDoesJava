86589f27f0f3c9c8cd2c971657777edfe365490a
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-07-28 19:01:44 +0530

    Update word break problem.java

--- Tries/word break problem.java
+++ Tries/word break problem.java
@@ -1,1 +1,20 @@
+public static boolean wordBreak(String key) {
+       int len = key.length();
+
+
+       if(len == 0) {
+           return true;
+       }
+
+
+       for(int i=1; i<=len; i++) {
+           if( search(key.substring(0, i)) &&
+               wordBreak(key.substring(i)) ) {
+                   return true;
+               }
+       }
+
+
+       return false;
+   }
 
