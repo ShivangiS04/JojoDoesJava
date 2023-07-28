0057ff4cffe4d7ae7c004511acfcd631909cc7d0
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-07-28 18:55:29 +0530

    Update and rename Implementations, Insert & Search.java to Insert.java

---
+++ Tries/Insert.java
@@ -0,0 +1,36 @@
+public class Tries {
+   static class Node {
+       Node[] children = new Node[26];
+       boolean eow;
+
+
+       public Node() {
+           for (int i=0; i<26; i++) {
+               children[i] = null;
+           }
+       }
+   }
+
+
+   public static Node root = new Node();
+
+
+   public static void insert(String word) { //O(n)
+       int level = 0;
+       int len = word.length();
+       int idx = 0;
+
+
+       Node curr = root;
+       for(; level<len; level++) {
+           idx = word.charAt(level)-'a';
+           if(curr.children[idx] == null) {
+               curr.children[idx] = new Node();
+           }
+           curr = curr.children[idx];
+       }
+       curr.eow = true;
+   }
+
+
+  
