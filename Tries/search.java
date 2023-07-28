2c7c0d1c60fb6283185617aedf02abbead2d0135
Shivangi Singh <122040215+ShivangiS04@users.noreply.github.com>
2023-07-28 18:56:29 +0530

    Update search.java

--- Tries/search.java
+++ Tries/search.java
@@ -1,1 +1,36 @@
+ public static boolean search(String key) { // Time complexity for the code =O(n)
+       int level = 0;
+       int len = key.length();
+       int idx = 0;
+
+
+       Node curr = root;
+       for(; level<len; level++) {
+           idx = key.charAt(level)-'a';
+           if(curr.children[idx] == null) {
+               return false;
+           }
+           curr = curr.children[idx];
+       }
+       return curr.eow == true;
+   }
+
+
+   public static void main(String args[]) {
+       String words[] = {"the", "a", "there", "their", "any", "thee"};
+       for (String word : words) {
+           insert(word);
+           System.out.println("inserted " + word);
+       }
+
+
+       System.out.println("thee -> " + search("thee"));
+       System.out.println("thor -> " + search("thor"));
+
+
+       System.out.println(startsWith("the"));
+       System.out.println(startsWith("thi"));
+   }
+}
+
 
